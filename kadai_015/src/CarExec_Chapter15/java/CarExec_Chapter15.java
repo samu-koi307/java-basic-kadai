27b80dc06b08bca98cba3c813effb6eaf9a21668
samu-koi307 <167383835+samu-koi307@users.noreply.github.com>
2024-04-18 11:28:58 +0900

    車クラスを作って速度を出力しました。

---
+++ kadai_015/src/CarExec_Chapter15/java/CarExec_Chapter15.java
@@ -0,0 +1,16 @@
+package CarExec_Chapter15.java;
+
+import Car_Chapter15.java.Car_Chapter15;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+
+		Car_Chapter15 car= new Car_Chapter15(1, 10);
+
+		car.gearChange(3);
+
+		car.run();
+
+	}
+}
